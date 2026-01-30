8e229292efe18fb7e447e99a1eed83db6031e9f8
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-31 01:29:47 +0530

    Implement alternative power of three check
    Added an alternative method to check if a number is a power of three.

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -12,5 +12,17 @@ class Solution {
             }
         }
         return true;
+
+        //OR 
+        if(n<=0){
+            return false;
+        }
+         while(n>=1){
+             if(n%3 !=0){
+                 return false;
+             }
+             n = n/3;
+         }
+        return true;
     }
 }
