7b35f1eda799940ceac263ed30d250c09d3ffd2d
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-31 01:23:14 +0530

    Add alternative method to check power of two
    Added an alternative implementation for checking power of two.

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -12,5 +12,17 @@ class Solution {
             }
         }
         return true;
+
+//OR 
+        if(n<=0){
+            return false;
+        }
+        while(n>=1){
+            if(n%2 !=0){
+                return false;
+            }
+            n = n/2;
+        }
+        return true;
     }
 }
