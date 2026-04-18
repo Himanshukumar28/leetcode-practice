ceb457a70a4f28bf1be80c35bdb4d5ee48f1bac2
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-04-18 23:12:31 +0530

    Time: 6 ms (17.79%) | Memory: 42.2 MB (36.03%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.java
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -1,12 +1,26 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
         //BINARY SEARCH
-        for(long a=0;a*a<=c;a++){
+        // for(long a=0;a*a<=c;a++){
+        //     double b = Math.sqrt(c-a*a);
+        //         if(b==(int)b){
+        //             return true;
+        //         }
+        // }
+        // return false;
+
+        // for(long a =0; a*a<=c; a++){
+        //     for(long b =0; b*b<=c; b++){
+        //         if(a*a + b*b == c) return true;
+        //     }
+        // }
+        // return false;  c= 2147483646
+
+        for(long a = 0; a*a <= c; a++){
             double b = Math.sqrt(c-a*a);
-                if(b==(int)b){
-                    return true;
-                }
-        }
+
+            if(b == (int)b) return true;
+        } 
         return false;
     }
 }
