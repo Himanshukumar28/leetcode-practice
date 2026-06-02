1c274e4b05660f8fc86a53a09ae1c497f3937664
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-06-02 23:35:10 +0530

    Time: 1 ms (100.00%) | Memory: 42.9 MB (26.26%) - LeetSync

--- 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.java
+++ 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.java
@@ -1,20 +1,19 @@
 class Solution {
     public long removeZeros(long n) {
-      long  rev= 0;
-     // long n1 = n;
-        while(n>=1){
-            long rem = n %10;
-            if(rem!=0){
-                rev = rev *10 + rem;
-            }
-            n = n/10;
+       long rev = 0;
+       while(n >= 1){
+        long rem = n % 10;
+        if(rem != 0){
+            rev = rev * 10 + rem;
         }
-        long rev1 = 0;
-        while(rev>=1){
-            long rem = rev%10;
-            rev1 = rev1 *10 + rem;
-            rev = rev/10;
-        }
-        return rev1;
+        n = n/10;
+       }
+       long rev1 = 0;
+       while(rev >= 1){
+        long rem = rev % 10;
+        rev1 = rev1 * 10 + rem;
+        rev = rev / 10;
+       } 
+       return rev1;
     }
 }
