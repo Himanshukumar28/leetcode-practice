dd75fb64550db2a484dc6cef5db720d814892064
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-06-03 22:39:33 +0530

    Time: 34 ms (7.52%) | Memory: 42.3 MB (94.52%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,11 +1,12 @@
 class Solution {
     public int arrangeCoins(int n) {
-        for(long i =1; i<=n; i++){
-            long sum = i*(i+1)/2;
+       long sum = 0;
+       for(long i = 1; i<=n; i++){
+         sum = i*(i+1)/2;
 
-            if(sum == n) return (int) i;
-            if(sum > n) return (int) i -1;
-        }
-        return 0;
+         if(sum == n) return(int) i;
+         if(sum > n) return(int) i-1;
+       }
+         return 0;
     }
 }
