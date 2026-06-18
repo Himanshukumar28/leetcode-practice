cdd023d6623c87d3efcf3bd25d707f19a07c935d
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-06-18 23:34:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (93.10%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.java
+++ 2491-smallest-even-multiple/smallest-even-multiple.java
@@ -1,18 +1,8 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        // int sint = 0;
-        // for(int i = n; i>=n; i++){
-        //     if(i%n==0 && i%2==0){
-        //         sint = sint + i;
-        //         break;
-        //     }
-        // }
-        // return sint;
-        if(n%2 ==0){
-            return n;
-        }
-        else{
-            return n*2;
-        }
+
+       if(n % 2 == 0) return n;
+       else return n*2;
+
     }
 }
