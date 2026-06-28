e33ca30ca323c49e542a7b8bf2ce1ded78721e45
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-06-28 16:34:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (12.75%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,17 +1,17 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m+1][n+1];
-        return count(1,1,m,n,dp);
-    }
-    public static int count(int sr, int sc, int er, int ec, int[][] dp){
-        if(sr > er || sc > ec) return 0;
-        if(sr == er || sc == ec) return 1;
-        if(dp[sr][sc] != 0) return dp[sr][sc];
-
-        //Right
-        int right = count(sr , sc+1 , er , ec , dp);
-        //Left
-        int left = count(sr+1 , sc , er , ec, dp);
-        return dp[sr][sc] =  right + left;
-    }
+        int[][] mat = new int[m][n];
+        for(int i = 0; i<n; i++){
+            mat[0][i] = 1;
+        }
+        for(int j = 0; j<m; j++){
+            mat[j][0] = 1;
+        }
+        for(int i = 1; i<m; i++){
+            for(int j = 1; j<n; j++){
+                mat[i][j] = mat[i][j-1] + mat[i-1][j];
+            }
+        }
+        return mat[m-1][n-1];
+    } 
 }
